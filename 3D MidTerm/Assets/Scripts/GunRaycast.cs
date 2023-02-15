ddb7c8e4c4c49f3aad8ce11d0f271f3cd9502624
DV <david_ventura_m@hotmail.com>
2023-02-15 00:24:58 -0500

    finilized the mid term

--- 3D MidTerm/Assets/Scripts/GunRaycast.cs
+++ 3D MidTerm/Assets/Scripts/GunRaycast.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunRaycast : MonoBehaviour
 {
@@ -9,22 +10,33 @@ public class GunRaycast : MonoBehaviour
     [SerializeField] float maxGrabDistance = 10f;
 
     public LayerMask activeLayers;
+    public LayerMask redDotLayer;
 
     bool isGrabed;
     Rigidbody grabbedObject;
-    [SerializeField] float forceIntensity = 500;    
+    //[SerializeField] float forceIntensity = 500;   
+    
+    public Text objectName;
+    public GameObject redDot;
+
+    Color oldColor = Color.black;
+    Renderer rendererHolder = null;
 
     private void Start()
     {
         isGrabed = false;
+        objectName.text = "No Object is grabed";
     }
 
     void Update()
     {//debug line so I can track where the raycast would appear
         Debug.DrawRay(gameObject.transform.position, transform.forward * maxRayDistance, Color.red);
 
+
+
+
             //shoot when left click
-        if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
         {
             Shoot();
         }
@@ -36,7 +48,38 @@ public class GunRaycast : MonoBehaviour
         }        
     }
 
-    //shoot method
+    private void LateUpdate()
+    {
+        //using late update to handle colors and the position of the red dot
+        Ray ray = new Ray(transform.position, transform.forward * maxRayDistance);
+        if (rendererHolder != null) 
+        {
+            rendererHolder.material.color = oldColor;
+        }
+
+        if (Physics.Raycast(ray, out RaycastHit hit, maxRayDistance, redDotLayer))
+        {
+            redDot.transform.position = hit.point;
+            if (hit.collider.CompareTag("grabable"))
+            {
+                rendererHolder = hit.collider.GetComponent<Renderer>();
+                oldColor = rendererHolder.material.color;
+                rendererHolder.material.color = Color.black;
+            }
+            if (hit.collider.CompareTag("hitable"))
+            {
+                rendererHolder = hit.collider.GetComponent<Renderer>();
+                oldColor = rendererHolder.material.color;
+                rendererHolder.material.color = Color.white;
+            }
+        }
+        else
+        {
+            redDot.transform.position = transform.forward * maxRayDistance;
+        }
+    }
+
+    //shoot method; destroys breakable objects
     void Shoot()
     {
         RaycastHit hit;
@@ -78,11 +121,12 @@ public class GunRaycast : MonoBehaviour
         }
     }
     
-    //if CheckForGrab comes out false this method gets called
+    //if CheckForGrab comes out false this method gets called; grabs an object and takes its name
     void Grab(RaycastHit hit, Ray ray)
     {
         if (Physics.Raycast(ray, out hit, maxRayDistance, activeLayers))
         {
+            objectName.text = (hit.collider.name);
             grabbedObject = hit.rigidbody;
             grabbedObject.isKinematic = true;
             grabbedObject.transform.SetParent(gameObject.transform);
@@ -90,11 +134,12 @@ public class GunRaycast : MonoBehaviour
         }
     }
 
-    //if CheckForGrab comes out true this method gets called
+    //if CheckForGrab comes out true this method gets called; grabs object and changes the grabed object text to "No Object is grabed"
     void Ungrab(RaycastHit hit, Ray ray)
     {
         if (Physics.Raycast(ray, out hit, maxRayDistance, activeLayers))
         {
+            objectName.text = "No Object is grabed";
             grabbedObject.transform.parent = null;
             grabbedObject.isKinematic = false;
             isGrabed = false;
